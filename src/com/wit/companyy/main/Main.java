513d6967af1bf22eb6cc6e6e83810cd47f717dfc
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-11-19 02:58:55 +0300

    POJOs chall 1

---
+++ src/com/wit/companyy/main/Main.java
@@ -0,0 +1,13 @@
+package com.wit.companyy.main;
+
+import com.wit.companyy.entity.Employee;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Employee> hdk = new LinkedList<>();
+    }
+}
