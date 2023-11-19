1d2c36c6d258fdf4b0ad6d886540d17053207dc9
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-11-20 02:08:28 +0300

    add Company Challange

--- src/com/wit/companyy/main/Main.java
+++ src/com/wit/companyy/main/Main.java
@@ -2,12 +2,46 @@ package com.wit.companyy.main;
 
 import com.wit.companyy.entity.Employee;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Employee> hdk = new LinkedList<>();
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(1,"Bilge", "Göksu"));
+        employees.add(new Employee(1,"Bilge", "Göksu"));
+        employees.add(new Employee(2,"SpongeBob", "SquarePants"));
+        employees.add(new Employee(2,"SpongeBob", "SquarePants"));
+        employees.add(new Employee(3,"Marilyne", "Monroe"));
+        employees.add(new Employee(3,"Marilyne", "Monroe"));
+        employees.add(new Employee(4,"Frank", "Sinatra"));
+        employees.add(new Employee(4,"Frank", "Sinatra"));
+        employees.add(null); // nullPointerException gelir 27. sırada. o yüzden employee != null && cehck'i eklemeliyim. ama o zaman da else de hata alırım.  direkt başka bir null if 'i ekliyorum en kısası.
+
+        // null bir data eklenmemesi üzerine çalışmalıdır! Buna Single Responsibility denir.
+
+        Map<Integer,Employee> empMap = new HashMap<>();
+        List<Employee> removeEmps = new ArrayList<>();
+
+        // ayrı bir metoda alabiliyorum bunu. if check 'i yapmam Single Responsibility 'ye uygun değil !!!
+        Iterator iterator = employees.iterator();
+        while(iterator.hasNext()){
+            Employee employee = (Employee) iterator.next();
+            if(employee == null) {
+                System.out.println("Find a NullPointerException !");
+                continue;
+            }
+            if(empMap.containsKey(employee.getId())){
+                removeEmps.add(employee);
+                iterator.remove();
+            } else{
+                empMap.put(employee.getId(),employee);
+            }
+        }
+        System.out.println("List: " + employees);
+        System.out.println("Map" + empMap);
+        System.out.println("Removed" + removeEmps);
+
+        // ConcurrentModificationException  alırız eğer FOR ile dönersek. List + Set ler için Iterator kullanıyorum: => // for (Employee employee: employees) {... employees.remove(employee);}
     }
 }
